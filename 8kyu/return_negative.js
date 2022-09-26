efec5d721d479ab2d41e8dbc004d4e7310d93bb0
RokPe <petkovicrok@gmail.com>
2022-09-26 22:13:30 +0200

    more codewars

--- 8kyu/return_negative.js
+++ 8kyu/return_negative.js
@@ -1,3 +1,9 @@
 function makeNegative(num) {
     return num < 0 ? num : -num
   }
+
+//alternative solution
+
+function makeNegative(num) {
+  return -Math.abs(num)
+}
--- 8kyu/sum_of_positive.js
+++ 8kyu/sum_of_positive.js
@@ -1,3 +1,9 @@
 function positiveSum(arr) {
     return arr.filter(x => x > 0).reduce((a, c) => a += c, 0)
   }
+
+//alternative solution
+
+function positiveSum(arr) {
+  return arr.reduce((a, c) => a += c > 0 ? c : 0, 0)
+}
