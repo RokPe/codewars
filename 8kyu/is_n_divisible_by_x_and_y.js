e58c630f79f0f0580598cfa3d2b89d543f8e1bb3
RokPe <petkovicrok@gmail.com>
2022-10-07 16:56:59 +0200

    codewars

--- 8kyu/is_n_divisible_by_x_and_y.js
+++ 8kyu/is_n_divisible_by_x_and_y.js
@@ -1,3 +1,6 @@
 function isDivisible(n, x, y) {
     return (n % x === 0 && n % y === 0) ? true : false
   }
+
+//one liner
+let isDivisible = (n, x, y) => (n % x === 0) && (n % y === 0) ? true : false
