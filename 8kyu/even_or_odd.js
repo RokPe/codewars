bfe9fcc77b2f763b05c63011d42571b9d1a44411
RokPe <petkovicrok@gmail.com>
2022-10-09 16:10:20 +0200

    codewars

--- 8kyu/even_or_odd.js
+++ 8kyu/even_or_odd.js
@@ -1,3 +1,6 @@
 function even_or_odd(number) {
   return number % 2 != 0 ? 'Odd' : 'Even'
 }
+
+//one liner
+let even_or_odd = number => number % 2 === 0 ? 'Even' : 'Odd'
--- 8kyu/string_repeat.js
+++ 8kyu/string_repeat.js
@@ -1,3 +1,6 @@
 function repeatStr (n, s) {
     return s.repeat(n)
   }
+
+//one liner
+let repeatStr = (n, s) => s.repeat(n)
