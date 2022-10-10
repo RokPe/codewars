c43ace8b1db7681f4824fc6566814b4e38352ae4
RokPe <petkovicrok@gmail.com>
2022-10-10 16:41:49 +0200

    codewars

--- 8kyu/abbreviate_a_two_word_name.js
+++ 8kyu/abbreviate_a_two_word_name.js
@@ -3,3 +3,10 @@ function abbrevName(name){
     let nameArr = name.split(' ').map(x => x.toUpperCase())
     return nameArr[0][0] + '.' + nameArr[1][0]
 }
+
+//alternative solution
+function abbrevName(name){
+
+    return name.split(' ').map(x => x[0].toUpperCase()).join('.')
+
+}
