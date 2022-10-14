99e414142a4bf59710cb23fd3a88415c345226f7
RokPe <petkovicrok@gmail.com>
2022-10-14 18:11:31 +0200

    codewars

--- 7kyu/disemvowel_trolls.js
+++ 7kyu/disemvowel_trolls.js
@@ -1,3 +1,8 @@
 function disemvowel(str) {
     return str.split('').filter(x => !'aeiouAEIOU'.includes(x)).join('')
   }
+
+//regex solution
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '')
+}
