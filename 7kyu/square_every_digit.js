9f4bd740646fcba1cc8621577cf37dd38fc12b85
RokPe <petkovicrok@gmail.com>
2022-10-15 17:50:48 +0200

    codewars

--- 7kyu/square_every_digit.js
+++ 7kyu/square_every_digit.js
@@ -1,3 +1,6 @@
 function squareDigits(num){
     return Number(num.toString().split('').map(x => Math.pow(Number(x), 2)).join(''))
   }
+
+//one liner
+let squareDigits = num => parseInt(num.toString().split('').map(x => Math.pow(parseInt(x), 2)).join(''))
