4bb44ac01b290b4731c2f636d6471b373781d8f4
RokPe <petkovicrok@gmail.com>
2022-10-16 17:36:39 +0200

    codewars

--- 7kyu/highest_and_lowest.js
+++ 7kyu/highest_and_lowest.js
@@ -1,3 +1,6 @@
 function highAndLow(numbers){
     return `${Math.max(...numbers.split(' '))} ${Math.min(...numbers.split(' '))}`
   }
+
+//one liner
+let highAndLow = numbers => `${Math.max(...numbers.split(' '))} ${Math.min(...numbers.split(' '))}`
