61449caedcb0243dc0828a56f3abbfc6f039f3e0
RokPe <petkovicrok@gmail.com>
2022-10-20 14:42:28 +0200

    codewars

--- 7kyu/get_the_middle_character.js
+++ 7kyu/get_the_middle_character.js
@@ -6,3 +6,13 @@ function getMiddle(s)
     return `${s.split('')[Math.floor(s.length/2 - 1)]}${s.split('')[Math.floor(s.length/2)]}`
   }
 }
+
+//alternative solution
+function getMiddle(s)
+{
+  if(s.length % 2 !== 0) {
+    return s[Math.floor(s.length/2)]
+  }else {
+    return s.slice(s.length/2 - 1, s.length/2 + 1)
+  }
+}
