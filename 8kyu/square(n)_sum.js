f90d36feec1575eeded877119e9a8bdac1665cc9
RokPe <petkovicrok@gmail.com>
2022-10-21 14:30:34 +0200

    codewars

--- 8kyu/square(n)_sum.js
+++ 8kyu/square(n)_sum.js
@@ -4,3 +4,8 @@ function squareSum(numbers){
 
 //one liner
 let squareSum = numbers => numbers.reduce((a, c) => a += Math.pow(c, 2), 0)
+
+//alternative
+function squareSum(numbers){
+  return numbers.reduce((a, c) => a += c * c, 0)
+}
