83e0b7f688087c40de5239487f7b9e3875d3c1e2
RokPe <petkovicrok@gmail.com>
2022-10-22 14:07:02 +0200

    codewars

--- 7kyu/isograms.js
+++ 7kyu/isograms.js
@@ -2,3 +2,10 @@ function isIsogram(str){
     let arr = str.split('').map(x => x.toLowerCase())
     return arr.filter(x => arr.indexOf(x) === arr.lastIndexOf(x)).length === str.length ? true : false
   }
+
+//improved
+function isIsogram(str){
+    let strArr = str.split('').map(x => x.toLowerCase())
+    return strArr.filter(x => strArr.indexOf(x) === strArr.lastIndexOf(x)).length === strArr.length
+  }
+  
