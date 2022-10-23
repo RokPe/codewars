7e08a8129a50d9aeaf23d22ec9b27cc101b2c7a9
RokPe <petkovicrok@gmail.com>
2022-10-23 13:54:23 +0200

    codewars

--- 7kyu/descending_order.js
+++ 7kyu/descending_order.js
@@ -1,3 +1,8 @@
 function descendingOrder(n){
     return Number(n.toString().split('').map(x => Number(x)).sort((a, b) => b - a).join(''))
   }
+
+//alternative solution
+function descendingOrder(n){
+  return +n.toString().split('').sort((a, b) => b - a).join('')
+}
